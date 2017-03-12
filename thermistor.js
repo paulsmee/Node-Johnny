d9db9e8a1162400ab40e7973b4304960042c9af9
Paul <paulsmee.is@gmail.com>
2017-03-12 16:46:47 +1100

    Updated

--- thermistor.js
+++ thermistor.js
@@ -1,6 +1,6 @@
 var j5 = require("johnny-five"),
 board, photoresistor;
-var temporal = require("temporal"); // NEW var for Servo!!!!!!
+var temporal = require("temporal");
 
 var board = new j5.Board();
 
@@ -63,10 +63,7 @@ function convertVoltageToLux(lux) {
 board.on("ready", function(){
   var thm = new j5.Sensor({ pin: "A0", freq: 500 });
   photoresistor = new j5.Sensor({ pin: "A2", freq: 5000 });
-
-// START NEW CODE !!!!!!!!!
   var servo = new j5.Servo({ pin: 10, startAt: 90 });
-// END NEW CODE !!!!!!!!!!
 
   board.repl.inject({
     pot: photoresistor
@@ -74,7 +71,6 @@ board.on("ready", function(){
 
   photoresistor.on("data", function(light1) {
     console.log(light1);
-   // var light = howMuchLight(light1); // This is no longer used
     var luxCalc = convertVoltageToLux(light1);
 
     stats.currentLightLux = light1; // I added this because i want a scale in my homekit app :)
@@ -85,9 +81,6 @@ board.on("ready", function(){
   thm.on("change",  function(thmVoltage) {
 
     var temp = convertVoltToTemp(thmVoltage);
-
-    // We don't want to replace the entire currentTemp object, we just want to update the values on the object.
-    // If we replaced the entire object the links to the file where this one is included would stop working.
 
     stats.currentTemp = temp.tempK;
     console.log("Current TempK: ", stats.currentTemp);

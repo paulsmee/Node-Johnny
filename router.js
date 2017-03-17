de5ba0fffe805daae267c0500361533f1dbf8388
Paul <paulsmee.is@gmail.com>
2017-03-17 18:27:14 +1100

    update

--- router.js
+++ router.js
@@ -2,12 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var stats = require('./sensor-light' && './sensor-temp');
-
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
+var stats = require('./sensors/sensor-light' && './sensors/sensor-temp' && './sensors/sensor-servo' && './remotes/remote-tv');
 
 router.get('/', function(req, res) {
   res.sendFile(__dirname + '/index1.html');
@@ -20,6 +15,41 @@ router.get('/get-light', function(req, res) {
 
 router.get('/get-temp', function(req, res) {
   res.send(stats.currentTemp+'');
+})
+
+router.get('/wave', function(req, res) {
+  console.log("got wave");
+  res.send(stats.wave());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Power Request");
+  res.send(remotecontrol.powerOn());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Request");
+  res.send(remotecontrol.volumeUp());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Request");
+  res.send(remotecontrol.volumeDown());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Request");
+  res.send(remotecontrol.channelUp());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Request");
+  res.send(remotecontrol.channelDown());
+})
+
+router.get('/remote', function(req, res) {
+  console.log("TV Request");
+  res.send(remotecontrol.mute());
 })
 
 module.exports = router

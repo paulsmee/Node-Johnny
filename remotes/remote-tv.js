bfcfc9e269901d3c4d94d54775beae7953b884d1
Paul <paulsmee.is@gmail.com>
2017-03-25 09:50:09 +0000

    update

--- remotes/remote-tv.js
+++ remotes/remote-tv.js
@@ -7,27 +7,27 @@ nodeLIRC.init();
 const remotecontrol = {};
 
 function power() {
-  nodeLIRC.send("LG_AKB72915207power", "POWER");
+  nodeLIRC.send("LG_AKB72915207", "KEY_POWER");
 };
 
 function volumeUp() {
-  nodeLIRC.send("LG_AKB72915207vup", "KEY_VOLUMEUP");
+  nodeLIRC.send("LG_AKB72915207", "KEY_VOLUMEUP");
 };
 
 function volumeDown() {
-  nodeLIRC.send("LG_AKB72915207vdown", "KEY_VOLUMEDOWN");
+  nodeLIRC.send("LG_AKB72915207", "KEY_VOLUMEDOWN");
 };
 
 function channelUp() {
-  nodeLIRC.send("LG_AKB72915207cup", "KEY_CHANNELUP");
+  nodeLIRC.send("LG_AKB72915207", "KEY_CHANNELUP");
 };
 
 function channelDown() {
-  nodeLIRC.send("LG_AKB72915207cdown", "KEY_CHANNELDOWN");
+  nodeLIRC.send("LG_AKB72915207", "KEY_CHANNELDOWN");
 };
 
 function mute() {
-  nodeLIRC.send("LG_AKB72915207mute", "MUTE");
+  nodeLIRC.send("LG_AKB72915207", "KEY_MUTE");
 };
 
 remotecontrol.power = power;
